4a115713a0d396b48462e1b7f536e32436f765fe
Todd Wade <waveright@gmail.com>
2022-11-13 09:03:37 -0500

    put core use()s in here

--- lib/WaveRight.pm
+++ lib/WaveRight.pm
@@ -2,4 +2,9 @@ use strict;
 use warnings;
 package WaveRight;
 
+use Data::FormValidator ();
+use Data::FormValidator::Constraints ();
+use Data::FormValidator::Multi ();
+use DateTime::Format::Strptime ();
+
 1;
